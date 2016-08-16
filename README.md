9fa2e8530a619644d4b549ef8eef06f2bce4aaca
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-08-17 01:48:57 +0200

    This is gonna be fun.

---
+++ README.md
@@ -0,0 +1,24 @@
+# gulp-csso-usage
+
+This is a simple plugin (also my first plugin) I scratched together while working on a static website.
+
+Given the HTML files, it creates a usage data JSON for [CSSO](https://github.com/css/csso).
+
+### Example usage
+
+```javascript
+var gulp = require('gulp');
+var usage = require('gulp-csso-usage');
+var concat = require('gulp-concat');
+
+gulp.task('usage', function() {
+  return gulp.src('**/*.html')
+    .pipe(concat('all.html'))
+    .pipe(usage)
+    .pipe(gulp.dest('.'));
+});
+```
+
+This should create `all.json` file containing usage data in CSSO specified format. Skipping the concat part will result in separate usage files generated, because that may be something you wish for.
+
+**Please bear in mind this is my first plugin and it might on the off chance destroy everything you've ever created. And I haven't had a chance to read the guidelines just yet.**
---
+++ index.js
@@ -0,0 +1,35 @@
+var Transform = require('readable-stream/transform');
+var htmlparser = require('htmlparser');
+var gutil = require('gulp-util');
+
+module.exports = new Transform({objectMode: true, transform: function(file, encoding, callback) {
+  // TODO: cross the streams
+  let usage = {tags: new Set(), classes: new Set(), ids: new Set()};
+  let check_tag = function (tag) {
+    if (tag.type == 'tag') {
+      usage.tags.add(tag.name);
+      if (tag.children)
+        for (let child of tag.children)
+          check_tag(child);
+      if (tag.attribs) {
+        if (tag.attribs['class'])
+          for (let tag_class of tag.attribs['class'].trim().split(/\s/))
+            usage.classes.add(tag_class);
+        if (tag.attribs['id'])
+          usage.ids.add(tag.attribs['id'].trim());
+      }
+    }
+  };
+  let htmlp = new htmlparser.Parser(new htmlparser.DefaultHandler());
+  if (file instanceof Buffer) {
+    htmlp.parseComplete(file.toString());
+  } else {
+    if (file.isNull()) return callback(null, file);
+    htmlp.parseComplete(file.contents.toString());
+  }
+  for (let tag of htmlp._handler.dom)
+      check_tag(tag);
+  file.path = gutil.replaceExtension(file.path, '.json');
+  file.contents =  new Buffer(JSON.stringify({tags: Array.from(usage.tags), classes: Array.from(usage.classes), ids: Array.from(usage.ids)}));
+  return callback(null, file);
+}});
